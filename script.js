659e0c803198ea998fad265f7b84615d7c58b93e
Geko 222 <110772386+Geko222@users.noreply.github.com>
2024-08-15 15:50:58 +0200

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.getElementById('triggerImage').addEventListener('click', function() {
+    // Ocultar la imagen que activa el video
+    var triggerImage = document.getElementById('triggerImage');
+    triggerImage.style.display = 'none';
+
     // Mostrar el contenedor del video
     var videoContainer = document.getElementById('videoContainer');
     videoContainer.style.display = 'block';
@@ -6,4 +10,10 @@ document.getElementById('triggerImage').addEventListener('click', function() {
     // Obtener el video y reproducirlo
     var videoPlayer = document.getElementById('videoPlayer');
     videoPlayer.play();
+
+    // Mostrar la imagen nuevamente cuando el video termine
+    videoPlayer.addEventListener('ended', function() {
+        triggerImage.style.display = 'block';
+        videoContainer.style.display = 'none'; // Ocultar el contenedor del video
+    });
 });
